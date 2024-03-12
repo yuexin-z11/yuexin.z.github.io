7eb111b5f33159aaa1084d846335b917ec7c3b30
yuexin zhang <teresayuexinzhang@gmail.com>
2024-03-12 02:57:24 -0500

    running out of ideas so DONE

--- content/resume.js
+++ content/resume.js
@@ -1,55 +1,3 @@
-// // show all 
-// filter("all")
-
-// // the function for the selection
-// function filter(category) {
-//   var x, i;
-//   x = document.getElementsByClassName("c")
-//   if (category == "all") category = "";
-//   // selections
-//   for (i = 0; i < x.length; i++){
-//     removeC(x[i], "show");
-//     if (x[i].className.indexOf(category) > -1) addC(x[i], "show");
-//   }
-// }
-
-// // show filtered elements
-// function addC(element, name){
-//   var i, a1, a2;
-//   a1 = element.className.split(" ");
-//   a2 = name.split(" ");
-//   for (i = 0; i < a2.length; i++){
-//     if (a1.indexOf(a2[i]) == -1) {
-//       element.className += " " + a2[i];
-//     }
-//   }
-// }
-
-// // hide elements
-// function removeC(element, name){
-//   var i, a1, a2;
-//   a1 = element.className.split(" ");
-//   a2 = name.split(" ");
-//   for (i = 0; i < a2.length; i++){
-//     if (a1.indexOf(a2[i]) == -1) {
-//       a1.splice(a1.indexOf(a2[i], 1));
-//     }
-//   }
-//   element.className = a1.join(" ");
-// }
-
-// // highlight the class
-// var btnC = document.getElementById("allBtns");
-// var btns = btnC.getElementsByClassName("r-btn");
-// for (var i = 0; i < btns.length; i++){
-//   btns[i].addEventListener("click",function()){
-//     var current = document.getElementsByClassName("active");
-//     current[0].className = current[0].className.replace(" active", "");
-//     this.className += " active";
-//   }
-// }
-// Show all
-
 filter("all");
 
 // The function for the selection
@@ -80,7 +28,6 @@ function addC(element, name) {
 
 // Hide elements
 function removeC(element, name) {
-  console.log("Removing class", name, "from element:", element); // Debug statement
   var i, a1, a2;
   a1 = element.className.split(" ");
   a2 = name.split(" ");
@@ -94,15 +41,11 @@ function removeC(element, name) {
 
 // Highlight the class
 var btnC = document.getElementById("allBtns");
-if (btnC) {
-  var btns = btnC.getElementsByClassName("r-btn");
-  for (var i = 0; i < btns.length; i++) {
-    btns[i].addEventListener("click", function() {
-      var current = document.getElementsByClassName("active");
-      if (current.length > 0) {
-        current[0].classList.remove("active");
-      }
-      this.classList.add("active");
-    });
-  }
+var btns = btnC.getElementsByClassName("r-btn");
+for (var i = 0; i < btns.length; i++) {
+  btns[i].addEventListener("click", function() {
+    var current = document.getElementsByClassName("active");
+    current[0].className = current[0].className.replace(" active", "");
+    this.className += " active";
+  });
 }
